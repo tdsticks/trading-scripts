167b1edd59101b50db9e794cbe745af86f61594f
Nathan Smith <nathansmithjax@gmail.com>
2017-12-10 18:44:15 -0500

    Add files via upload

--- z_base_scanner.py
+++ z_base_scanner.py
@@ -23,7 +23,7 @@ open("alerts_deleted.txt", "a")
 
 ##############VARIABLES TO SET
 days = 60 #how far back to look for bases. Suggested at least 14
-skip = 6 #this is how many candles back from now we ignore when scanning for bases; has to be at least 6
+skip = 7 #this is how many candles back from now we ignore when scanning for bases; has to be at least 7
 market = "BTC"
 minutes = "60" #candle time interval. suggested 60 for regular base breaks
 drop = .95 #percentage below detected bases to set alert so .96 is 4% down. Suggest at least .96 or .95
@@ -33,9 +33,9 @@ low_or_close = 3 #whether you want bases based on the low or the close (bottom o
 split_the_difference = False #Detects bases at 50% of wicks rather than the low or close prices. Setting this to True will cause the low_or_close variable to be ignored
 delete_old_alerts = True #If you want to delete all old alerts in alerts_set.txt for a coin before creating a new alert for that coin
 if len(sys.argv) > 1:
-    exchange = sys.argv[1]
+    exchange = sys.argv[1].upper()
     minimum_volume = int(sys.argv[2])
-    market = sys.argv[3]
+    market = sys.argv[3].upper()
 else:
     exchange = "BTRX"
     minimum_volume = 1 #Filters out all coins whose volume (in base coin in the last 24h) is less than this amount
@@ -43,14 +43,19 @@ else:
 ###############VARIABLES TO SET
 
 
+
 if coinigykey == "---Your-coinigy-key-here---":
     input("You need to put your coinigy key and secret in api_keys.py")
     sys.exit()
 
+headers = {'Content-Type': 'application/json', 'X-API-KEY': coinigykey, 'X-API-SECRET': coinigysec}
+
+#get old alerts
+alerts = z_manage_alerts.AlertManager(coinigykey, coinigysec)
+old_alerts = alerts._get_old_alerts()
 
 def get_coins(exch_code):
     l = []
-    headers = {'Content-Type': 'application/json', 'X-API-KEY': coinigykey, 'X-API-SECRET': coinigysec}
     values = '{"exch_code": ' + exchange + '}'
     values = bytes(values, encoding='utf-8')
     request = Request('https://api.coinigy.com/api/v1/markets', data=values, headers=headers)
@@ -65,7 +70,6 @@ def get_coins(exch_code):
     return l
 
 def get_coin_price_volume(exchange_code, coin_slash_market):
-    headers = {'Content-Type': 'application/json', 'X-API-KEY': coinigykey, 'X-API-SECRET': coinigysec}
     values = '{"exchange_code": "' + exchange_code + '","exchange_market": "' + coin_slash_market  + '"}'
     values = bytes(values, encoding='utf-8')
     request = Request('https://api.coinigy.com/api/v1/ticker', data=values, headers=headers)
@@ -75,9 +79,6 @@ def get_coin_price_volume(exchange_code, coin_slash_market):
     request = json.loads(request)
     return request
 
-#get old alerts
-alerts = z_manage_alerts.AlertManager(coinigykey, coinigysec)
-old_alerts = alerts._get_old_alerts()
 
 def check_dup_alerts(coin, price):
     for x in old_alerts['data']['open_alerts']:
@@ -86,107 +87,166 @@ def check_dup_alerts(coin, price):
             return True
     return False
 
+def setalert(x, y):
+    print("Base at candle " + str(x))
+    print("Base price " + str(round(decimal.Decimal(y), 8)))
+    print("Alert price " + str(round(decimal.Decimal(y * drop), 8)))
+    print("Volume %f" % vol_base)
+    #print(min(l))
 
-coins = get_coins(exchange)
-for coin in coins:
-    #coin = "GRS"
-    print("\n" + coin)
-    #input("Press enter to continue")
-    vol_base = get_coin_price_volume(exchange, coin + '/' + market)
-    try:
-        last_price = float(vol_base['data'][0]['last_trade'])
-        vol_base = float(vol_base['data'][0]['current_volume']) * float(vol_base['data'][0]['last_trade'])
-    except:
-        print(vol_base)
-        continue
-    if vol_base < minimum_volume:
-        continue
-    print(str(vol_base) + " volume")
+    #Delete old alerts for this coin
+    if delete_old_alerts:
+        alerts_deleted = open("alerts_deleted.txt").readlines()
+        for line in open("alerts_set.txt"):
+            if len(line) > 1 and line.split("\t")[5].strip() == coin + "/" + market and line.split("\t")[4].strip() == exchange and not line in alerts_deleted:
+                open("alerts_deleted.txt", "a").write(line)
+                notification_id = line.split("\t")[1].strip()
+                delete_coins = z_manage_alerts.AlertManager(coinigykey, coinigysec)
+                delete_coins._api_delete_alert(notification_id)
+                time.sleep(1)
 
-    a = urlopen("https://www.coinigy.com/getjson/chart_feed/" + 
-                 exchange + "/" + 
-                 coin + "/" + 
-                 market + "/" + 
-                 minutes + "/" + 
-                 str(round(time.time()-(int(86400 * days)))) + "/" + 
-                 str(round(time.time()))).read().decode("utf-8"
-                 )
-
+    values = '{"exch_code": "' + exchange + '", "market_name": "' + coin + '/' + market + '", "alert_price": ' + str(y * drop) + ', "alert_note": ""}'
+    values = bytes(values, encoding='utf-8')
+    request = Request('https://api.coinigy.com/api/v1/addAlert', data=values, headers=headers)
+    response_body = urlopen(request).read()
     time.sleep(2)
-    b = json.loads(a)
+    print(response_body)
+    response_body = response_body.decode("utf-8")
+    response_body = json.loads(response_body)
+    new_alert = alerts._get_old_alerts()['data']['open_alerts'][-1]
+    time.sleep(2)
+    print(new_alert)
+    if new_alert['mkt_name'] == coin + '/' + market:
+        open("alerts_set.txt", "a").write(
+                                          str(time.time()) + "\t" + 
+                                          new_alert["alert_id"] + "\t" + 
+                                          new_alert["alert_added"] + "\t" +
+                                          new_alert["price"] + "\t" +
+                                          new_alert["exch_code"] + "\t" +
+                                          new_alert["mkt_name"] + "\n"
+                                      )
 
-    c = [x[low_or_close] for x in b]
-    if split_the_difference:
-        c = [str((float(x[3]) + float(x[4])) / 2) for x in b]
-    c = [float(x) for x in c]
 
-    l = []
+def sixup(c, x, y):                        
+    strikes = 0
+    if (x < skip or x > len(c) - 13): return
+    l.append(y)
+    if (c[x - 1] >= y and
+        c[x + 1] >= y and
+        c[x - 2] >= y and
+        c[x + 2] >= y and
+        c[x - 3] > y and
+        c[x + 3] > y and
+        c[x + 4] > y and
+        c[x + 5] > y and
+        c[x + 7] > y and
+        c[x + 8] > y and
+        c[x + 9] > y and
+        c[x + 10] > y and
+        c[x + 11] > y and
+        c[x + 12] > y and
+        y <= min(l) and
+        (c[x - 6] >= y * six_candle_up or c[x - 7] >= y * six_candle_up) and
+        c[x - 6] - y > 0.00000002 and #To eliminate false positives for low sat coins like DOGE and RDD
+        (c[x + 6] >= y * six_candle_up or c[x + 7] >= y * six_candle_up) and
+        c[x + 6] - y > 0.00000002
+        ):
+            if c[x - 3] < c[x - 1]: strikes += 1
+            if c[x - 4] < c[x - 2]: strikes += 1
+            if c[x - 5] < c[x - 3]: strikes += 1
+            if c[x + 3] < c[x + 1]: strikes += 1
+            if c[x + 4] < c[x + 2]: strikes += 1
+            if c[x + 5] < c[x + 3]: strikes += 1
+            print(str(strikes) + " strike(s) against potential base at candle " + str(x))
+            if strikes <= sensitivity:
+                dupe = check_dup_alerts(coin + "/" + market, y * drop)
+                if dupe:
+                    return "nextcoin"
+                if not dupe and last_price > y * drop:
+                    setalert(x, y)
+                    return "nextcoin"
 
-    for x, y in enumerate(c):
-        strikes = 0
-        if (x < skip or x > len(c) - (skip + 1)): continue
-        l.append(y)
-        if (c[x - 1] >= y and
-            c[x + 1] >= y and
-            c[x - 2] >= y and
-            c[x + 2] >= y and
-            c[x - 3] > y and
-            c[x + 3] > y and
-            y <= min(l) and
-            c[x - 6] >= y * six_candle_up and
-            c[x - 6] - y > 0.00000002 and #To eliminate false positives for low sat coins like DOGE and RDD
-            c[x + 6] >= y * six_candle_up and
-            c[x + 6] - y > 0.00000002
-            ):
-                if c[x - 3] < c[x - 1]: strikes += 1
-                if c[x - 4] < c[x - 2]: strikes += 1
-                if c[x - 5] < c[x - 3]: strikes += 1
-                if c[x + 3] < c[x + 1]: strikes += 1
-                if c[x + 4] < c[x + 2]: strikes += 1
-                if c[x + 5] < c[x + 3]: strikes += 1
-                print(str(strikes) + " strike(s) against potential base at candle " + str(x))
-                if strikes <= sensitivity:
-                    dupe = check_dup_alerts(coin + "/" + market, y * drop)
-                    if dupe:
-                        break
-                    if not dupe and last_price > y * drop:
-                        print("Base at candle " + str(x))
-                        print("Base price " + str(round(decimal.Decimal(y), 8)))
-                        print("Alert price " + str(round(decimal.Decimal(y * drop), 8)))
-                        print("Volume %f" % vol_base)
-                        #print(min(l))
+def avgthree(c, x, y):
+    #print(x)
+    #print(y)
+    strikes = 0
+    if (x < skip or x > len(c) - (13)): return
+    l.append(y)
+    if (c[x - 1] >= y and
+        c[x + 1] >= y and
+        c[x - 2] >= y and
+        c[x + 2] >= y and
+        c[x - 3] > y and
+        c[x + 3] > y and
+        c[x - 4] > y and
+        c[x + 4] > y and
+        c[x - 5] > y and
+        c[x + 5] > y and
+        c[x + 7] > y and
+        c[x + 8] > y and
+        c[x + 9] > y and
+        c[x + 10] > y and
+        c[x + 11] > y and
+        c[x + 12] > y and
+        y <= min(l) and
+        c[x - 6] >= y * six_candle_up and
+        c[x - 6] - y > 0.00000002 and #To eliminate false positives for low sat coins like DOGE and RDD
+        c[x + 6] >= y * six_candle_up and
+        c[x + 6] - y > 0.00000002 and
+        (c[x - 1] + c[x - 2] + c[x - 3]) / 3 < (c[x - 2] + c[x - 3] + c[x - 4]) / 3 < (c[x - 4] + c[x - 5] + c[x - 6]) / 3 and
+        (c[x + 1] + c[x + 2] + c[x + 3]) / 3 < (c[x + 2] + c[x + 3] + c[x + 4]) / 3 < (c[x + 4] + c[x + 5] + c[x + 6]) / 3
+        ):
+            print(str(strikes) + " strike(s) against potential base at candle " + str(x))
+            if strikes <= sensitivity:
+                dupe = check_dup_alerts(coin + "/" + market, y * drop)
+                if dupe:
+                    return "nextcoin"
+                if not dupe and last_price > y * drop:
+                    setalert(x, y)
+                    return "nextcoin"
 
-                        #Delete old alerts for this coin
-                        if delete_old_alerts:
-                            alerts_deleted = open("alerts_deleted.txt").readlines()
-                            for line in open("alerts_set.txt"):
-                                if len(line) > 1 and line.split("\t")[5].strip() == coin + "/" + market and line.split("\t")[4].strip() == exchange and not line in alerts_deleted:
-                                    open("alerts_deleted.txt", "a").write(line)
-                                    notification_id = line.split("\t")[1].strip()
-                                    delete_coins = z_manage_alerts.AlertManager(coinigykey, coinigysec)
-                                    delete_coins._api_delete_alert(notification_id)
-                                    time.sleep(1)
+if __name__ == "__main__":
+    coins = get_coins(exchange)
+    for coin in coins:
+        #coin = "DYN"
+        print("\n" + coin)
+        #input("Press enter to continue")
+        vol_base = get_coin_price_volume(exchange, coin + '/' + market)
+        try:
+            last_price = float(vol_base['data'][0]['last_trade'])
+            vol_base = float(vol_base['data'][0]['current_volume']) * float(vol_base['data'][0]['last_trade'])
+        except:
+            print(vol_base)
+            continue
+        if vol_base < minimum_volume:
+            continue
+        print(str(vol_base) + " volume")
 
-                        values = '{"exch_code": "' + exchange + '", "market_name": "' + coin + '/' + market + '", "alert_price": ' + str(y * drop) + ', "alert_note": ""}'
-                        values = bytes(values, encoding='utf-8')
-                        headers = {'Content-Type': 'application/json','X-API-KEY': coinigykey, 'X-API-SECRET': coinigysec}
-                        request = Request('https://api.coinigy.com/api/v1/addAlert', data=values, headers=headers)
-                        response_body = urlopen(request).read()
-                        time.sleep(2)
-                        print(response_body)
-                        response_body = response_body.decode("utf-8")
-                        response_body = json.loads(response_body)
-                        new_alert = alerts._get_old_alerts()['data']['open_alerts'][-1]
-                        time.sleep(2)
-                        print(new_alert)
-                        if new_alert['mkt_name'] == coin + '/' + market:
-                            open("alerts_set.txt", "a").write(
-                                                              str(time.time()) + "\t" + 
-                                                              new_alert["alert_id"] + "\t" + 
-                                                              new_alert["alert_added"] + "\t" +
-                                                              new_alert["price"] + "\t" +
-                                                              new_alert["exch_code"] + "\t" +
-                                                              new_alert["mkt_name"] + "\n"
-                                                          )
-                        break
-#input("FINISHED")
+        candles = urlopen("https://www.coinigy.com/getjson/chart_feed/" + 
+                     exchange + "/" + 
+                     coin + "/" + 
+                     market + "/" + 
+                     minutes + "/" + 
+                     str(round(time.time()-(int(86400 * days)))) + "/" + 
+                     str(round(time.time()))).read().decode("utf-8"
+                     )
+
+        time.sleep(2)
+        candles = json.loads(candles)
+
+        if split_the_difference:
+            candles = [(float(x[3]) + float(x[4])) / 2 for x in candles]
+        else:
+            candles = [float(x[low_or_close]) for x in candles]
+
+
+        l = []
+
+
+        for x, y in enumerate(candles):
+            if sixup(candles, x, y) == "nextcoin":
+                print("sixup")
+                break
+            if avgthree(candles, x, y) == "nextcoin":
+                print("avgthree")
+                break
