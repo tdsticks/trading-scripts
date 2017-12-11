b40d8b1538f4c2c84cb0e950cc1a9eff7f48665f
Nathan Smith <nathansmithjax@gmail.com>
2017-12-11 11:23:56 -0500

    Add files via upload

--- z_alerts.py
+++ z_alerts.py
@@ -1,37 +1,39 @@
 #!/usr/bin/python3
 
-from urllib.request import urlopen
 import time
+import bittrex
 import os
 import decimal
 import json
+import requests
+try:
+    import z_manage_alerts
+except Exception as e:
+    print(e)
+    input("You need to download z_manage_alerts.py from the project site")
+    sys.exit()
 
-os.system("./z_get_alerts.php")
-for line in open("alerts.txt"): print(line)
 
 while True:
-    summaries = urlopen("https://bittrex.com/api/v1.1/public/getmarketsummaries").read().decode("utf-8")
-    summaries = json.loads(summaries)
-    alerts = open("alerts.txt").readlines()
-    for alert in alerts:
-        alert = alert.split()
-        alert[2] = float(alert[2].strip())
-        for summary in summaries['result']:
-            if alert[0] == summary['MarketName']:
-                short_name = summary['MarketName'].split("-")[1]
-                #print(summary['MarketName'])
-                if alert[1] == '>' and summary['Last'] > alert[2]:
-                    print()
-                    print(summary['MarketName'] + " above " + str(decimal.Decimal(alert[2])))
-                    print("https://www.coinigy.com/main/markets/BTRX/" + short_name + "/BTC")
-                    print("https://bittrex.com/Market/Index?MarketName=BTC-" + short_name)
+    coinigy_alerts = z_manage_alerts.AlertManager._get_old_alerts()
+    btrx_summaries = requests.get("https://bittrex.com/api/v1.1/public/getmarketsummaries")
+    btrx_summaries = json.loads(btrx_summaries.text)
+
+    for alert in coinigy_alerts['data']['open_alerts']:
+        alert['price'] = float(alert['price'])
+        coinigy_mkt_name = alert['mkt_name'].split('/')[1] + '-' + alert['mkt_name'].split('/')[0]
+        for summary in btrx_summaries['result']:
+            if coinigy_mkt_name == summary['MarketName']:
+                if alert['operator'] == '>' and summary['Last'] > alert['price']:
+                    print("\n" + summary['MarketName'] + " above " + str(round(decimal.Decimal(alert['price']), 8)))
+                    print("https://www.coinigy.com/main/markets/BTRX/" + alert['mkt_name'])
+                    print("https://bittrex.com/Market/Index?MarketName=" + summary['MarketName'])
                     os.system("mpv os.mp3  >/dev/null 2>&1")
-                    os.system("./get_alerts.php")
-                if alert[1] == '<' and summary['Last'] < alert[2]:
-                    print()
-                    print(summary['MarketName'] + " below " + str(decimal.Decimal(alert[2])))
-                    print("https://www.coinigy.com/main/markets/BTRX/" + short_name + "/BTC")
-                    print("https://bittrex.com/Market/Index?MarketName=BTC-" + short_name)
+
+                if alert['operator'] == '<' and summary['Last'] < alert['price']:
+                    print("\n" + summary['MarketName'] + " below " + str(round(decimal.Decimal(alert['price']), 8)))
+                    print("https://www.coinigy.com/main/markets/BTRX/" + alert['mkt_name'])
+                    print("https://bittrex.com/Market/Index?MarketName=" + summary['MarketName'])
                     os.system("mpv os.mp3  >/dev/null 2>&1")
-                    os.system("./get_alerts.php")
-    time.sleep(15)
+
+    time.sleep(15) #Do not lower this below 2 or coinigy might end up banning you
